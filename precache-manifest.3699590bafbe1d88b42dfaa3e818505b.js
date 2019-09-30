b984218e81044e8a09b23f05859de72ef2f03caf
lozzoc <reach.maddaloc@gmail.com>
2019-09-29 19:23:31 -0700

    Updates

---
+++ precache-manifest.3699590bafbe1d88b42dfaa3e818505b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf84ef795f814e6ae868e4aaacdabcc",
+    "url": "/lozzoc/index.html"
+  },
+  {
+    "revision": "3e350ab8fa12b7af204e",
+    "url": "/lozzoc/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "0f8a7968863634654cc8",
+    "url": "/lozzoc/static/css/main.3fde77f4.chunk.css"
+  },
+  {
+    "revision": "3e350ab8fa12b7af204e",
+    "url": "/lozzoc/static/js/2.376b318d.chunk.js"
+  },
+  {
+    "revision": "0f8a7968863634654cc8",
+    "url": "/lozzoc/static/js/main.af9b8ea2.chunk.js"
+  },
+  {
+    "revision": "79e5c2a45628db2aaf0a",
+    "url": "/lozzoc/static/js/runtime-main.68d5b4e2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6e3011fcd99c207b0bebd20d4ae12ecd.js"
+  "/lozzoc/precache-manifest.3699590bafbe1d88b42dfaa3e818505b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lozzoc/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
